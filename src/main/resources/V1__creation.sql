74e2fe5d30367e21bc53bbe18a52cb9c0833ea28
Yurii <borlok@bk.ru>
2021-02-13 23:46:49 +0300

    +travis

--- src/main/resources/V1__creation.sql
+++ src/main/resources/V1__creation.sql
@@ -1,19 +1,19 @@
 create table if not exists users
 (
-    id serial primary key
+    id int auto_increment primary key
 );
 
 create table if not exists events
 (
-    id      serial primary key,
-    user_id int ,
+    id      int auto_increment primary key,
+    user_id int,
     name    varchar(255),
     foreign key (user_id) references users (id)
 );
 
 create table if not exists accounts
 (
-    id             serial primary key,
+    id             int auto_increment primary key,
     user_id        int UNIQUE,
     name           varchar(50) DEFAULT 'UNNAMED',
     account_status varchar(20) default 'ACTIVE',
@@ -22,7 +22,7 @@ create table if not exists accounts
 
 create table if not exists files
 (
-    id          serial primary key,
+    id          int auto_increment primary key,
     user_id     int,
     file_status varchar(20) default 'ACTIVE',
     foreign key (user_id) references users (id)
@@ -30,11 +30,11 @@ create table if not exists files
 
 create table if not exists access
 (
-    id serial primary key,
-    email varchar(50) not null,
-    first_name varchar (50) not null,
-    last_name varchar(50) not null,
-    password varchar (255) not null,
-    role varchar(20) not null default 'USER',
-    status varchar (20) not null default 'ACTIVE'
+    id         int auto_increment primary key,
+    email      varchar(50)  not null,
+    first_name varchar(50)  not null,
+    last_name  varchar(50)  not null,
+    password   varchar(255) not null,
+    role       varchar(20)  not null default 'USER',
+    status     varchar(20)  not null default 'ACTIVE'
 )
--- src/main/resources/V2__populate.sql
+++ src/main/resources/V2__populate.sql
@@ -1,11 +1,14 @@
-insert into users (id) values (1);
-insert into accounts (id, user_id, name, account_status) VALUES (1,1,'Yura', 'ACTIVE');
-insert into files (id, user_id, file_status) values (1,1,'ACTIVE');
-insert into events (id,user_id, name) VALUES (1,1,'Miting');
-insert into access (id,email,first_name,last_name,password,role,status)
- VALUES (1,'admin@mail.com','Admin','Adminov',
+insert into users (id) value (1);
+insert into accounts (user_id, name, account_status) value (1,'Yura', 'ACTIVE');
+insert into files ( user_id, file_status) value (1,'ACTIVE');
+insert into events (user_id, name) value (1,'Miting');
+insert into access (email,first_name,last_name,password,role,status)
+ VALUES ('admin@mail.com','Admin','Adminov',
          '$2y$12$PEiyzJ0PePdlFfX3jJAwgOGG1nMdMZyEXCYWwsaozWx1C97YHfF7u',
          'ADMIN','ACTIVE'),
-        (2,'user@mail.com','User','Userov',
+        ('moderator@mail.com','Moderator','Moderatorov',
+         '$2y$12$sirxc28pnbeSD8crVfSHNuWHPRymc2MyJC4uJMcR96wcDWAIL7Qx.',
+         'MODERATOR','ACTIVE'),
+        ('user@mail.com','User','Userov',
          '$2y$12$Ud1AwtXOJ21faUqk4FSXS.mtooIIDLWYyfEb0JXkeUYiqAaC4lS8q',
-         'USER','BANNED');
+         'USER','ACTIVE');
